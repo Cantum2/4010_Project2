d45d153d2a7dfb086491d7285cfcb2c7f978bea4
Connor L Kasarda <47127368+ckasarda@users.noreply.github.com>
2020-04-19 13:01:48 -0600

    Added concept code to driver
    This could be start to writing, reading diagonal of csv data.

--- driver.cpp
+++ driver.cpp
@@ -101,3 +101,26 @@ void writeHashToCSV(string fileName, string value){
   cout<<"end f"<<endl;
   
 }
+
+// Possible concept for writing (and reading) diagonal of csv data
+  // Writes a diagonal of 1's in csv with 0's in upper right and lower left cells.
+  // Assumes that variables specified by rows, not columns (my bad, I can change that).
+/**
+ @param filename The name of the file to create or update
+ */
+void write_diagonal(string filename) {
+    fstream fout;
+    fout.open(filename, ios::out | ios::app);
+    int startRow = 0, rowWidth = 10;
+    int i = 0, diag = 0;
+    for(diag = 0; diag < rowWidth - 1; diag++) {        
+        for(i = 0; i < diag; i++) { // i = 0..(d - 1)
+            fout << 0 << ','; // Insert random value
+        }
+        fout << 1; // i = d, Insert desired value on diagonal 
+        for(i = diag + 1; i < rowWidth - 1; i++) { // i = (d + 1)..rowWidth
+            fout << ", " << 0;// Insert random value
+        }
+        fout << "\n";
+    }
+}
