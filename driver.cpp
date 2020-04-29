19d89b7fd186cc36c4d7e16ab1f0554e46fe4e03
Connor L Kasarda <47127368+ckasarda@users.noreply.github.com>
2020-04-29 12:33:22 -0600

    Made changes to write diag hash function
    Now it takes filename and vector of strings, and hashes in diagonals in csv.
    
    THIS IS NOT FULLY FUNCTIONING YET!
    
    Also, I still have the old function in there commented out just in case.

--- driver.cpp
+++ driver.cpp
@@ -5,8 +5,6 @@
 #include <sstream>
 #include "Encoder.h"
 
-
-
 using namespace std;
 
 const string FILE_NAME = "output.csv";
@@ -64,25 +62,43 @@ vector<string> readRow(istream& str){
   return row; 
 }
 
-// Possible concept for writing (and reading) diagonal of csv data
-  // Writes a diagonal of 1's in csv with 0's in upper right and lower left cells.
-  // Assumes that variables specified by rows, not columns (my bad, I can change that).
+// Takes in updated variable data and filename, then
+// updates the file using trunc constant to discard
+// the old data before updating
 /**
  @param filename The name of the file to create or update
+ @param hashes the vector of variable data to hash and insert diagonally
  */
-void writeHashToCSV(string filename) {
+void writeHashToCsv(string filename, vector<string> hashes) {
+  fstream fout;
+  fout.open(filename, ios::out | ios::trunc);
+  int maxLength = 0;
+  // Get length of longest string
+  for (size_t v = 0; v < hashes.size(); v++) {
+    if (int tempLength = hashes.at(v).length() > maxLength) {
+      maxLength = tempLength;
+    }
+  }
+  int offset = 0;
+  for (size_t i = 0; i < maxLength; i++) {
+    for (size_t j = 0; j < maxLength; j++) {
+      // Place hashes.at(j).at(i) @ cell [j + offset][i]
+    }
+    offset += 1;
+  }
+}
+
+/*
+void writeHashToCSV(string filename, vector<string> updateData) {
     fstream fout;
-    fout.open(filename, ios::out | ios::app);
+    fout.open(filename, ios::out | ios::trunc);
     int startRow = 0, rowWidth = 10;
     int i = 0, diag = 0;
     for(diag = 0; diag < rowWidth - 1; diag++) {        
-        for(i = 0; i < diag; i++) { // i = 0..(d - 1)
-            fout << 0 << ','; // Insert random value
-        }
-        fout << 1; // i = d, Insert desired value on diagonal 
-        for(i = diag + 1; i < rowWidth - 1; i++) { // i = (d + 1)..rowWidth
-            fout << ", " << 0;// Insert random value
-        }
+        for(i = 0; i < diag; i++) fout << 0 << ", ";
+        fout << 1;
+        for(i = diag + 1; i < rowWidth - 1; i++) fout << ", " << 0;
         fout << "\n";
     }
 }
+*/
